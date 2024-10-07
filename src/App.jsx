6ba55d37071f9e4f52bdd43a39fa400e34ca5cfe
Your Name <your-email@example.com>
2024-10-07 11:47:34 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './components/Weather'
+
+const App = () => {
+  return (
+    <div>
+      <div className='app'>
+        <Weather />
+      </div>
+    </div>
+  )
+}
+
+export default App
